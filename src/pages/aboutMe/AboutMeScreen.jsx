abfa99d53c40eeeebed4f8a8a79cf1d00f7aaf41
Ada George <adageorge79@gmail.com>
2023-02-27 19:56:38 -0600

    responsive about

--- src/pages/aboutMe/AboutMeScreen.jsx
+++ src/pages/aboutMe/AboutMeScreen.jsx
@@ -7,7 +7,7 @@ const AboutMeScreen = () => {
   return (
     <>
       <header className='relative overflow-hidden w-screen h-64 max-sm:h-52 -z-10 bg-[url("https://www.goodfreephotos.com/albums/other-landscapes/pink-sunset-on-the-beach-of-castillo-en-el-mar.jpg")] bg-no-repeat bg-cover bg-center drop-shadow-[0_0_3px_rgba(0,0,0,0.25)]'>
-        <div className="absolute bottom-7 max-sm:left-2 max-lg:left-10 lg:left-20 lg:ml-20 font-sans">
+        <div className="absolute bottom-7 max-sm:left-2 left-6 xl:left-40 lg:ml-10 font-sans">
           <h3 className="text-4xl max-sm:text-2xl text-black bg-p-clear opacity-50 max-sm:py-2 max-sm:px-10 py-5 px-32 rounded-md">
             ABOUT ME
           </h3>
--- src/pages/aboutMe/CardFreedom.jsx
+++ src/pages/aboutMe/CardFreedom.jsx
@@ -9,9 +9,9 @@ const CardFreedom = () => {
     <div className="absolute right-9 border-2 border-p-clear-2 border-solid h-96 w-96 rotate-45"></div>
     <div className="absolute right-0 border-2 border-p-clear-2 border-solid h-96 w-96 rotate-45"></div>
 
-    <div className="p-2 flex max-sm:flex-col-reverse max-w-screen-md m-auto bg-p-clear-2 rounded-lg shadow-[0_0_15px_rgba(0,0,0)]">
-      <div className="md:bg-m-clear h-80 max-sm:h-60 relative md:left-16 flex flex-col self-center justify-center">
-        <p className="text-8xl max-sm:text-6xl font-dancing absolute top-6 max-sm:top-3 text-white drop-shadow-[0_0_3px_rgba(0,0,0,0.25)]">
+    <div className="p-2 flex max-md:flex-col-reverse max-w-screen-md m-auto bg-p-clear-2 rounded-lg shadow-[0_0_15px_rgba(0,0,0)]">
+      <div className="md:bg-m-clear h-80 max-md:h-60 relative md:left-16 flex flex-col self-center justify-center">
+        <p className="text-8xl max-sm:text-6xl font-dancing absolute top-6 max-md:top-3 text-white drop-shadow-[0_0_3px_rgba(0,0,0,0.25)]">
           Freedom
         </p>
         <p className="font-sans p-6 pt-10 md:pr-10">
